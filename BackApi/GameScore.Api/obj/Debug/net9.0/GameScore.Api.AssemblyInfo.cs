a331b305fe7441e254485ad7578e8240a585c592
Maycon Pereira <mayconpereira77@gmail.com>
2025-04-04 19:09:54 -0300

    .

--- BackApi/GameScore.Api/obj/Debug/net9.0/GameScore.Api.AssemblyInfo.cs
+++ BackApi/GameScore.Api/obj/Debug/net9.0/GameScore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameScore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec0eb0ae2d1b467f1ec2ba10f4c3ae91c65af7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f6a6951e6fb014ca8037fed5e5edec9a3e2aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameScore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameScore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FrontApp/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Login } from './pages/Login/Login'
+import { Menu } from './pages/Menu/Menu'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/Menu' element={<Menu/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
